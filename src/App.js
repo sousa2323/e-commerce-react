590dc9f892e3a4cd66adc8d599b2fcf33cd7b763
arthur <sousanot.23@gmail.om>
2021-08-05 20:50:43 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            E-Commerce
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Products/Poduct/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconBotton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+
+const Product = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Product

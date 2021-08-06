b0be7cc24cbc06fc9ee1d6aa2bef11b72a891696
arthur <sousanot.23@gmail.om>
2021-08-06 20:17:26 -0300

    modification

--- src/components/Products/Poduct/Product.jsx
+++ src/components/Products/Poduct/Product.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typography, IconBotton, IconButton } from '@material-ui/core';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
